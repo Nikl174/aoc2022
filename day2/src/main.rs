7af41387baf1587a0bc4dc81d773a9a12721095c
Nikl <nstarost81@gmail.com>
2022-12-03 12:47:02 +0100

    finished part 2 of day 2

--- day2/src/main.rs
+++ day2/src/main.rs
@@ -3,6 +3,7 @@ use std::fs::File;
 use std::io::{BufRead, BufReader};
 
 /* CHEAT SHEET
+ * Part 1:
  * A, X - Rock
  * B, Y - Paper
  * C, Z - Scissors
@@ -11,13 +12,24 @@ use std::io::{BufRead, BufReader};
  * B, Z - Win
  * C, X - Win
  * ...
- *   XYZ 
- * A 360 
- * B 036 
- * C 603 
+ *   XYZ
+ * A 360
+ * B 036
+ * C 603
+ *
+ * Part 2:
+ * X - Loose
+ * Y - Draw
+ * Z - Win
+ *   XYZ
+ * A 312
+ * B 123
+ * C 231
  */
 
-const VALUE_MATRIX: [[usize; 3]; 3] = [[3, 6, 0], [0, 3, 6], [6, 0, 3]];
+const VALUE_MATRIX_P1: [[usize; 3]; 3] = [[3, 6, 0], [0, 3, 6], [6, 0, 3]];
+const POINT_MATRIX: [usize; 3] = [0, 3, 6];
+const VALUE_MATRIX_P2: [[usize; 3]; 3] = [[3, 1, 2], [1, 2, 3], [2, 3, 1]];
 const A: usize = 0;
 const B: usize = 1;
 const C: usize = 2;
@@ -27,8 +39,13 @@ const Z: usize = 2;
 
 fn main() {
     let file = get_puzzle_input().expect("Couldn't read file!");
-    let result = file.lines().fold(0, calc_points);
-    println!("TotalPoints: {}",result);
+    let result_part1 = file.lines().fold(0, calc_points_part1);
+    let file = get_puzzle_input().expect("Couldn't read file!");
+    let result_part2 = file.lines().fold(0, calc_points_part2);
+    println!(
+        "TotalPoints Part 1: {}; TotalPoints Part 2: {}",
+        result_part1, result_part2
+    );
 }
 
 // -------Utillity functions--------
@@ -59,7 +76,7 @@ fn get_puzzle_input() -> Result<BufReader<File>, std::io::Error> {
 // -------Utillity functions--------
 
 // -------Fold function-------
-fn calc_points(point_sum: usize, new_points: Result<String, std::io::Error>) -> usize {
+fn calc_points_part1(point_sum: usize, new_points: Result<String, std::io::Error>) -> usize {
     let round = new_points.expect("Line read error");
     let mut round_iterator = round.chars();
 
@@ -69,10 +86,26 @@ fn calc_points(point_sum: usize, new_points: Result<String, std::io::Error>) -> 
 
     let enemy_index = convert(enemy).expect("Conversion Error!");
     let player_index = convert(player).expect("Conversion Error!");
-    println!("Player: {player}; Enemy: {enemy}; MatrixVal: {}",VALUE_MATRIX[enemy_index][player_index]);
 
-    let round_sum =  player_index + 1 + VALUE_MATRIX[enemy_index][player_index];
-    println!("Roundsum: {round_sum}; Current Sum: {}",round_sum+point_sum);
+    let round_sum = player_index + 1 + VALUE_MATRIX_P1[enemy_index][player_index];
 
     return round_sum + point_sum;
 }
+
+fn calc_points_part2(point_sum: usize, new_points: Result<String, std::io::Error>) -> usize {
+    let round = new_points.expect("Line read error");
+    let mut round_iterator = round.chars();
+
+    let enemy = round_iterator.next().expect("Input Error");
+    round_iterator.next();
+    let player = round_iterator.next().expect("Input Error");
+
+    let enemy_index = convert(enemy).expect("Conversion Error!");
+    let player_index = convert(player).expect("Conversion Error!");
+
+    let round_sum = VALUE_MATRIX_P2[enemy_index][player_index] + POINT_MATRIX[player_index];
+    println!("Enemy: {enemy}; Player: {player}; Roundsum: {round_sum}");
+
+    return round_sum + point_sum;
+}
+
